4786668c2dac719b98d1bb076f0ffae29aa5fab9
GyeongHyeon <lkh6035@naver.com>
2021-08-01 20:04:12 +0900

    renew

--- at_project/urls.py
+++ at_project/urls.py
@@ -4,13 +4,15 @@ from at_project.views import *
 app_name = 'at_project'
 urlpatterns = [
     # 경현
-    path('k-food-report', IndexViews.as_view(), name='k-food-report'),
-    path('idol/', AboutViews.as_view(), name='idol'),
-    path('food-recipe-2/', AboutViews.as_view(), name='food-recipe-2'),
-    path('k-tour/', AboutViews.as_view(), name='k-tour'),
+    path('k-food-report/', KFoodReportViews.as_view(), name='k-food-report'),
+    path('recipe-ajax/', gy_views.recipe_ajax, name='recipe-ajax'),
+    path('idol/', KFoodIdolViews.as_view(), name='idol'),
+    path('food-recipe-2/', FoodRecipe2Views.as_view(), name='food-recipe-2'),
+    path('test/', TableauTestViews.as_view(), name='idol'),
+    # path('k-tour/', AboutViews.as_view(), name='k-tour'),
 
     # 지원
-    path('', '', name='index'),
-    path('k-food-report-graph', '', name='k-food-report-graph'),
-    path('food-recipe/', '', name='food-recipe'),
+    # path('', '', name='index'),
+    # path('k-food-report-graph/', '', name='k-food-report-graph'),
+    # path('food-recipe/', '', name='food-recipe'),
 ]
--- at_project/views/gy_views.py
+++ at_project/views/gy_views.py
@@ -1,46 +1,57 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.http import HttpResponse, JsonResponse
+from bs4 import BeautifulSoup
+import requests
 
 
-class IndexViews(TemplateView):
-    template_name = "at_project/gy/index.html"
+class JSONResponseMixin:
+    """
+    A mixin that can be used to render a JSON response.
+    """
+
+    def render_to_json_response(self, context, **response_kwargs):
+        """
+        Returns a JSON response, transforming 'context' to make the payload.
+        """
+        return JsonResponse(
+            self.get_data(context),
+            **response_kwargs
+        )
+
+    def get_data(self, context):
+        """
+        Returns an object that will be serialized as JSON by json.dumps().
+        """
+        # Note: This is *EXTREMELY* naive; in reality, you'll need
+        # to do much more complex handling to ensure that arbitrary
+        # objects -- such as Django model instances or querysets
+        # -- can be serialized as JSON.
+        return context
 
 
-class AboutViews(TemplateView):
-    template_name = "at_project/gy/about.html"
+class KFoodReportViews(TemplateView):
+    template_name = "at_project/gy/k-food-report.html"
 
 
-class BlogViews(TemplateView):
-    template_name = "at_project/gy/blog.html"
+def recipe_ajax(request):
+    if request.is_ajax():
+        test = request.GET
+        _url = ''.join([f'{value}={test[value]}' if  idx == 0 else f'&{value}={test[value]}' for idx, value in enumerate(test)])
+        url = f'http://api.nongsaro.go.kr/service/{_url}'
+        res = requests.get(url)
+        soup = BeautifulSoup(res.content, 'html.parser')
+        return HttpResponse(soup,
+                            content_type='application/json')
+    return render(request, 'at_project/gy/repice.html')
 
 
-class BlogDetailViews(TemplateView):
-    template_name = "at_project/gy/blog-details.html"
+class KFoodIdolViews(TemplateView):
+    template_name = "at_project/gy/k-food-idol.html"
 
 
-class CheckoutViews(TemplateView):
-    template_name = "at_project/gy/checkout.html"
+class FoodRecipe2Views(TemplateView):
+    template_name = "at_project/gy/food-recipe-2.html"
 
-
-class ClassViews(TemplateView):
-    template_name = "at_project/gy/class.html"
-
-
-class ContactViews(TemplateView):
-    template_name = "at_project/gy/contact.html"
-
-
-class ShopViews(TemplateView):
-    template_name = "at_project/gy/shop.html"
-
-
-class ShopDetailViews(TemplateView):
-    template_name = "at_project/gy/shop-details.html"
-
-
-class ShopingCartViews(TemplateView):
-    template_name = "at_project/gy/shoping-cart.html"
-
-
-class WissListViews(TemplateView):
-    template_name = "at_project/gy/wisslist.html"
+class TableauTestViews(TemplateView):
+    template_name = "at_project/gy/tableau_test.html"
